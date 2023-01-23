bbc979ca2fe8117002e4f56464a05c740d29c40e
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-01-23 18:27:39 -0300

    Create VoteContract.sol
    A contract to vote throght blockchain.

---
+++ contract.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Contract {
+	enum Choices { Yes, No }
+	
+	struct Vote {
+		Choices choice;
+		address voter;
+	}
+
+	Vote none = Vote(Choices(0), address(0));
+
+	Vote[] public votes; 
+
+	function createVote(Choices choice) external {
+		require(!hasVoted(msg.sender));
+		votes.push(Vote(choice, msg.sender));
+	}
+
+	function changeVote(Choices choice) external {
+		Vote storage vote = findVote(msg.sender);
+		require(vote.voter != none.voter);
+		vote.choice = choice;
+	}
+
+	function findVote(address voter) internal view returns(Vote storage) {
+		for(uint i = 0; i < votes.length; i++) {
+			if(votes[i].voter == voter) {
+				return votes[i];
+			}
+		}
+		return none;
+	}
+
+	function hasVoted(address voter) public view returns(bool) {
+		return findVote(voter).voter == voter;
+	}
+
+	function findChoice(address voter) external view returns(Choices) {
+		return findVote(voter).choice;
+	}
+}
